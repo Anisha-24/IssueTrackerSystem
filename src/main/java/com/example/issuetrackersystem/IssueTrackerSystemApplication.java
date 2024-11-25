0113bc02f9d19a915fe918659f3a9c1c7a509af5
Anisha-24 <anisha242002@gmail.com>
2024-11-25 12:30:31 +0530

    Source Code

---
+++ src/main/java/com/example/issuetrackersystem/IssueTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.issuetrackersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueTrackerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/issuetrackersystem/IssueTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issuetrackersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
